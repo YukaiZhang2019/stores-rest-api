0eb7d4c8d1e1b98e52110cab438c4c34299132bc
Yukai Zhang <yukai.zhang@mail.mcgill.ca>
2020-09-08 14:04:26 -0400

    First commit, added REST API to access items, stores, and user authentication.

---
+++ models/user.py
@@ -0,0 +1,58 @@
+# import sqlite3
+from db import db
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    # These props created in the __init__ function must match to those here below
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        # id props will be auto-filled
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        '''
+        connection = sqlite3.connect('data.db')    
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,)) 
+        row = result.fetchone() 
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        
+        connection.close()
+        return user
+        '''
+        # SELECT * FROM users
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        '''
+        connection = sqlite3.connect('data.db')    
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,)) 
+        row = result.fetchone() 
+        if row:
+            user = cls(*row)
+        else:
+            user = none
+        
+        connection.close()
+        return user
+        '''
+        return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
